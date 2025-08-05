a2876cf576357b166c1cfa4ac8709392284b0eab
M1RAK <abdullahiismail1105@gmail.com>
2025-08-05 14:01:10 +0100

    added webhook to the isPublic route.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/clerk(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if(!isPublicRoute(req)){
-    await auth.protect()
+  if (!isPublicRoute(req)) {
+    await auth.protect();
   }
 });
 
